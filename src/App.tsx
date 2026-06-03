b65f2d896da5ea95d88e2d23f96db000f525ba47
rxored <rxored@gmail.com>
2026-06-03 23:41:27 +0530

    enterthevoid

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-black flex flex-col items-center">
+      <img
+        src="/WhatsApp_Image_2026-05-25_at_20.43.52.jpeg"
+        alt="Enter The Void - Event Poster"
+        className="w-full max-w-[430px] block"
+      />
+      <img
+        src="/WhatsApp_Image_2026-05-25_at_21.18.35.jpeg"
+        alt="Enter The Void - Lineup Poster"
+        className="w-full max-w-[430px] block"
+      />
+    </div>
+  );
+}
+
+export default App;
